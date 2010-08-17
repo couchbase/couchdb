5f65a80ca8d687541f657e55ad048d7f4e7803d9
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-08-17 01:56:19 -0700

    Variable pagination.

--- share/www/sammy/app.js
+++ share/www/sammy/app.js
@@ -13,7 +13,12 @@ window.app = app;
 
 var isEven = function (someNumber) {
     return (someNumber%2 == 0) ? true : false;
-};
+}
+var sum = function (arr) {
+  var s = 0;
+  for (var i=0;i<arr.length;i+=1) s += arr[i];
+  return s;
+}
 var formatSize = function (size) {
   var jump = 512;
   if (size < jump) return size + " bytes";
@@ -25,92 +30,116 @@ var formatSize = function (size) {
   }
   return size.toFixed(1) + ' ' + units[i - 1];
 }
+
+var request = function (options, callback) {
+  options.success = function (obj) {
+    callback(null, obj);
+  }
+  options.error = function (err) {
+    if (err) callback(err);
+    else callback(true);
+  }
+  options.dataType = 'json';
+  $.ajax(options)
+}
   
 app.index = function () {
-  $('h1#topbar').append('<strong>Overview</strong>');
+  $('h1#topbar').html('<strong>Overview</strong>');
   var dbRow = function (name, even) {
     var row = $('<tr id=db-"'+name+'"><th><a href="#/'+name+'">'+name+'</a></th></tr>');
     
     row.addClass(even ? "even" : "odd")    
     row.appendTo('tbody.content');
-    $.ajax({ dataType: 'json', url: '/'+name 
-           , success: function (info) {              
-               row.append('<td class="size">'+formatSize(info.disk_size)+'</td>' +
-                          '<td class="count">'+info.doc_count+'</td>' + 
-                          '<td class="seq">'+info.update_seq+'</td>'
-                          )
-                          ;   
-           }
-           , error: function (info) {
-               row.append('<td class="size">error</td>' +
-                          '<td class="count">error</td>' +
-                          '<td class="seq">error</td>'
-                          )
-                          ;
-           }
+    request({url: '/'+name}, function ( err, info ) {
+      if (err) info = { disk_size:"can't connect", doc_count:"can't connect"
+                      , update_seq:"can't connect"};
+      row.append('<td class="size">'+formatSize(info.disk_size)+'</td>' +
+                  '<td class="count">'+info.doc_count+'</td>' + 
+                  '<td class="seq">'+info.update_seq+'</td>'
+                  );
     });
   }
   
-  var moreRows = function (dbs, start) {
-    for (var i=start;i<(start + 20);i+=1) { 
-       if (dbs[i]) dbRow(dbs[i], isEven(i));
-       else {$('span.more').remove(); return;}
+  
+  
+  request({url: '/_all_dbs'}, function (err, dbs) {
+    if (err) $('tbody.content').append('<tr><td>error</td><td>error</td><td>error</td></tr>');
+    else {
+      var moreRows = function (start, limit) {
+        for (var i=start;i<(start + limit);i+=1) { 
+           if (dbs[i]) dbRow(dbs[i], isEven(i));
+           else {$('div#pagination').remove(); return;}
+        }
+        $('span.more').unbind('click');
+        $('span.more').click(function ( ) { moreRows(i, parseInt($('#pages-input').val())) })
+      }
+      if (dbs.length > 20) {
+        var pagination = '<div id="pagination"><span class="more">Load </span><input type="text" id="pages-input" value=20></input><span class="more"> More Items</span></div>'
+         $('td.more').append(pagination);
+      }
+      moreRows(0, 20);
     }
-    $('span.more').unbind('click');
-    $('span.more').click(function ( ) { moreRows(dbs, i) })
-  }
+  })
 
-  $.ajax({ dataType: 'json', url: '/_all_dbs' 
-         , success: function (dbs) { 
-             if (dbs.length > 20) {
-               $('td.more').append('<span class="more">Load 20 More Items</span>');
-             }
-             moreRows(dbs, 0);
-         }
-         , error: function () {
-           // Add a good error message on the page. 
-         }    
-  });
 }
 app.showDatabase = function (db) {
-  $('h1#topbar').append('<strong>'+db+'</strong>');
+  $('h1#topbar').html('<a href="#/">Overview</a><strong>'+db+'</strong>');
+  request({url: '/'+db}, function (err, info) {
+    for (i in info) {$('div#'+i).text(info[i])}
+    var disk_size = info.disk_size;
+    $('div#disk_size').text(formatSize(info.disk_size))
+    
+    request({url:'/'+db+'/_all_docs?startkey="_design/"&endkey="design0"'}, function (err, docs) {
+      var sizes = [];
+      for (var i=0;i<docs.rows.length;i+=1) {
+        request({url:'/'+db+'/'+docs.rows[i].id+'/_info'}, function (err, info) {
+          sizes.push(info.view_index.disk_size);
+          if (sizes.length === docs.rows.length) {
+            var s = sum(sizes)
+            $('div#views_size').text(formatSize(s));
+            $('div#full_size').text(formatSize(s + disk_size));
+          }
+        })
+      }
+  })
+  
+    
+  })
   var rowCount = 0;
   var moreRows = function (start, limit) {
-    $.ajax({ dataType: 'json', url: '/'+db+'/_all_docs?limit='+limit+'&skip='+start
-           , success: function (resp) {
-             for (i in resp.rows) {
-               var r = resp.rows[i];
-               var row = $('<tr><td><a href="#/'+db+'/'+r.key+'">'+r.key+'</a></td><td>' +
-                            r.value.rev+'</td></tr>'
-                          )
-                          .addClass(isEven(rowCount) ? "even" : "odd")
-                          .appendTo('tbody.content')
-                          ;
-               rowCount += 1;
-             }
-             if (resp.total_rows > (resp.rows.length + start) && !$('span.more').length ) {
-               $('td.more').append('<span class="more">Load 20 More Items</span>');
-             } else if ( resp.total_rows <= (resp.rows.length + start) ) {
-               $('span.more').remove()
-             }
-             $('span.more').unbind('click');
-             $('span.more').click(function ( ) { moreRows(start + limit, limit) });
-           }
-           
-           })
+    request({url: '/'+db+'/_all_docs?limit='+limit+'&skip='+start}, function (err, resp) {
+      if (err) return;
+      for (i in resp.rows) {
+         var r = resp.rows[i];
+         var row = $('<tr><td><a href="#/'+db+'/'+r.key+'">'+r.key+'</a></td><td>' +
+                      r.value.rev+'</td></tr>'
+                    )
+                    .addClass(isEven(rowCount) ? "even" : "odd")
+                    .appendTo('tbody.content')
+                    ;
+         rowCount += 1;
+     }
+     if (resp.total_rows > (resp.rows.length + start) && !$('span.more').length ) {
+       $('td.more').append('<div id="pagination"><span class="more">Load </span><input type="text" id="pages-input" value='+limit+'></input><span class="more"> More Items</span></div>');
+     } else if ( resp.total_rows <= (resp.rows.length + start) ) {
+       console.log([resp.total_rows, resp.rows.length + start])
+       $('div#pagination').remove()
+     }
+     $('span.more').unbind('click');
+     $('span.more').click(function ( ) { moreRows(start + limit, parseInt($('#pages-input').val())) });
+   })
   }
-  moreRows(0, 20)
+  moreRows(0, 20);
 }
 
-$.sammy(function () {
+var a = $.sammy(function () {
   
   var indexRoute = function () {
     this.render('templates/index.mustache').replace('#content').then(app.index);
   }
   var databaseRoute = function () {
     var db = this.params['db'];
-    this.render('/'+db)
-        .render('templates/database.mustache')
+    this.render('templates/database.mustache')
         .replace('#content')
         .then( function () {app.showDatabase(db)} )
         ;
@@ -120,5 +149,6 @@ $.sammy(function () {
   this.get('', indexRoute);
   this.get("#/", indexRoute);
   this.get('#/:db', databaseRoute);
-}).run();
+})
 
+$(function () {a.run()});
