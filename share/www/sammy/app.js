e3ed432a58e2c6fd5ab9e9b332b6e7a155d9f8b9
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-08-18 22:43:29 -0700

    Added changes page. Better comments.

--- share/www/sammy/app.js
+++ share/www/sammy/app.js
@@ -21,8 +21,12 @@ var formatSize = function (size) {
   }
   return size.toFixed(1) + ' ' + units[i - 1];
 }
-var parseQuery = function (query) {
-  if (query[0] === '?') query = query.slice(1);
+var getQuery = function () {
+  if (window.location.hash.indexOf('?') !== -1) {
+    query = window.location.hash.slice(window.location.hash.indexOf('?')+1)
+  } else {
+    return;
+  }
   var s = query.split('&');
   var r = {}
   for (var i=0;i<s.length;i+=1) {
@@ -30,6 +34,13 @@ var parseQuery = function (query) {
     r[x.slice(0, x.indexOf('='))] = unescape(x.slice(x.indexOf('=')+1));
   }
   return r;
+}
+var queryToString = function (obj) {
+  var l = []
+  for (i in obj) {
+    l.push( i + '=' + escape(obj[i]) );
+  }
+  return l.join('&');
 }
 
 var request = function (options, callback) {
@@ -83,17 +94,15 @@ app.index = function () {
 }
 app.showDatabase = function () {
   var db = this.params['db']
-    , query
+    , query = getQuery()
     ;
   
-  if (window.location.hash.indexOf('?') !== -1) {
-    query = window.location.hash.slice(window.location.hash.indexOf('?'))
-  }
   
   var init = function () {
     $('h1#topbar').html('<a href="#/">Overview</a><strong>'+db+'</strong>');
 
     var addquery = function () {
+      // This function adds the _all_docs startkey/endkey query options
       $('select.dbquery-select').before(
         '<div class="alldoc-query">' + 
           '<span class="query-option">end<input class="query-option" id="end" type="text"></input></span>' +
@@ -104,30 +113,34 @@ app.showDatabase = function () {
         var startkey = $('input#start').val()
           , endkey = $('input#end').val()
           ;
+        // Check if the keys are properly json encoded as strings, if not do it 
         if (startkey[0] !== '"' && startkey.length !== 0) startkey = '"'+startkey+'"'
         if (endkey[0] !== '"' && endkey.length !== 0) endkey = '"'+endkey+'"'
-        
-         h = '#/'+db+'/_all_docs?';
-         if (startkey.length > 0) h += ('startkey='+escape(startkey) + '&');
-         if (endkey.length > 0) h += ( 'endkey='+escape(endkey) + '&');
-         window.location.hash = h;
+        // Craft query
+        h = '#/'+db+'/_all_docs?';
+        if (startkey.length > 0) h += ('startkey='+escape(startkey) + '&');
+        if (endkey.length > 0) h += ( 'endkey='+escape(endkey) + '&');
+        window.location.hash = h;
       });
     }
 
     request({url: '/'+db}, function (err, info) {
-
+      // Fill out all info from the db query.
       for (i in info) {$('div#'+i).text(info[i])}
-
       var disk_size = info.disk_size;
       $('div#disk_size').text(formatSize(info.disk_size))
-
+      
+      // Query for ddocs to calculate size
       request({url:'/'+db+'/_all_docs?startkey="_design/"&endkey="_design0"'}, function (err, docs) {
         var sizes = [];
         for (var i=0;i<docs.rows.length;i+=1) {
+          // Query every db for it's size info
+          // Note: because of a current bug this query sometimes causes a view update even with stale=ok
           request({url:'/'+db+'/'+docs.rows[i].id+'/_info?stale=ok'}, function (err, info) {
             if (err) throw err
             sizes.push(info.view_index.disk_size);
             if (sizes.length === docs.rows.length) {
+              // All queries are finished, update size info
               var s = sum(sizes)
               $('div#views_size').text(formatSize(s));
               $('div#full_size').text(formatSize(s + disk_size));
@@ -135,7 +148,8 @@ app.showDatabase = function () {
           })
         }
         if (docs.rows.length === 0) {
-          $('div#views_size').text(formatSize(disk_size));
+          // There are no design documents, db size is full size
+          $('div#views_size').text(formatSize(0));
           $('div#full_size').text(formatSize(disk_size));
         }
       })
@@ -143,53 +157,61 @@ app.showDatabase = function () {
 
     $('select.dbquery-select').change(function (e) {
       if (e.target.value === 'all') {
+        // All Documents selected, bounce back to dburl
         $('div.alldoc-query').remove();
         if (query) {window.location.hash = '#/'+db}
       } else if (e.target.value === 'ddocs') {
+        // Design doc was selected, pop out query and fill with ddoc query
         $('div.alldoc-query').remove();
         addquery();
         $('input#start').val('_design/');
         $('input#end').val('_design0').change();
       } else if (e.target.value === 'query') {
+        // Query selected, pop out query options
         $('div.alldoc-query').remove();
         addquery();
       }
     })
     if (query) {
-      var q = parseQuery(query);
-      if (q.startkey || q.endkey) {
+      if (query.startkey || query.endkey) {
+        // There is an open all docs query, pop out query options and fill in with current query
         $('option[value=all]').attr('selected', false);
         $('option[value=query]').attr('selected', true).change();
-        $('input#start').val(q.startkey ? q.startkey : '');
-        $('input#end').val(q.endkey ? q.endkey : '');
+        $('input#start').val(query.startkey ? query.startkey : '');
+        $('input#end').val(query.endkey ? query.endkey : '');
       }
     }
   } 
   
   var rowCount = 0;
   var moreRows = function (start, limit) {
+    // This function adds more rows to the current document table
     if (query) {
-      query += '&limit='+limit+'&skip='+start
+      query.limit = limit
+      query.skip = start
     } else {
-      query = '?limit='+limit+'&skip='+start
+      query = {limit:limit, skip:start}
     }
-    request({url: '/'+db+'/_all_docs'+query}, function (err, resp) {
+    request({url: '/'+db+'/_all_docs?'+queryToString(query)}, function (err, resp) {
       if (err) throw err;
-      for (i in resp.rows) {
-         var r = resp.rows[i];
-         var row = $('<tr><td><a href="#/'+db+'/'+r.key+'">'+r.key+'</a></td><td>' +
-                      r.value.rev+'</td></tr>'
-                    )
-                    .addClass(isEven(rowCount) ? "even" : "odd")
-                    .appendTo('tbody.content')
-                    ;
-         rowCount += 1;
+      for (var i=0;i<resp.rows.length;i+=1) {
+        row = $('<tr><td><a href="#/'+db+'/'+resp.rows[i].key+'">'+resp.rows[i].key+'</a></td><td>' +
+                 resp.rows[i].value.rev+'</td></tr>'
+               )
+               // rowCount currently breaks on odd pagination values
+               .addClass(isEven(rowCount) ? "even" : "odd")
+               .appendTo('tbody.content')
+               ;
+        rowCount += 1;
      }
-     if (resp.total_rows > (resp.rows.length + start) && !$('span.more').length ) {
+     if (!$('span.more').length && (resp.rows.length == limit) ) {
+       // The number of rows is less than the limit and we haven't added the pagination element yet 
        $('td.more').append('<div id="pagination"><span class="more">Load </span><input type="text" id="pages-input" value='+limit+'></input><span class="more"> More Items</span></div>');
-     } else if ( resp.total_rows <= (resp.rows.length + start) ) {
+     } else if ( resp.rows.length < limit ) {
+       // If the return rows are less than the limit we can remove pagination
        $('div#pagination').remove()
      }
+     // Remove the previous pagination handler and add a new one with the new closure values
      $('span.more').unbind('click');
      $('span.more').click(function ( ) { moreRows(start + limit, parseInt($('#pages-input').val())) });
    })
@@ -213,6 +235,8 @@ app.showDocument = function () {
   var db = this.params['db']
     , docid = this.params['docid']
     ;
+  $('h1#topbar').html('<a href="#/">Overview</a><a href="#/'+db+'">'+db+'</a><strong>'+docid+'</strong>');  
+  // This is a terrible hack to get the old document UI mostly working
   this.render('templates/document.mustache', {db:db,docid:docid}).replace('#content').then(
     $.getScript('script/base64.js', function() {
     $.getScript('script/jquery.resizer.js?0.11.0', function() {
@@ -255,16 +279,48 @@ app.showDocument = function () {
   )
 }
 
+app.showChanges = function () {
+  var db = this.params['db'];
+  $('h1#topbar').html('<a href="#/">Overview</a><a href="#/'+db+'">'+db+'</a><strong>_changes</strong>');  
+  this.render('templates/changes.mustache').replace('#content').then(function () {
+    var query = getQuery()
+      , url = '/'+db+'/_changes'
+    if (query) url += ('?' + queryToString(query))
+    request({url:url}, function (err, resp) {
+      // Render the response in 10 row chunks for efficiency
+      var pending = []
+        , renderPending = function () {$('table#changes').append(pending.join('')); pending = [];}
+        , c , changes
+        , baseUrl = '#/'+db+'/'
+        ;
+      for (var i=0;i<resp.results.length;i+=1) {
+        c = resp.results[i]; 
+        changes = []
+        $.each(c.changes, function (x, change) {
+          changes.push(change.rev);
+        })
+        pending.push('<tr><td>'+c.seq+'</td><td><a href="'+baseUrl+c.id+'">'+c.id+'</a><td>['+changes.join(', ')+']</td></tr>')
+        if (pending.length > 10) renderPending();
+      }
+      renderPending()
+    })
+  })
+}
+
 var a = $.sammy(function () {
   
   var indexRoute = function () {
     this.render('templates/index.mustache').replace('#content').then(app.index);
   }
-  
+  // Index of all databases
   this.get('', indexRoute);
   this.get("#/", indexRoute);
+  // Database view
   this.get('#/:db', app.showDatabase);
   this.get('#/:db/_all_docs', app.showDatabase);
+  // Database _changes feed
+  this.get('#/:db/_changes', app.showChanges);
+  // Document editor/viewer
   this.get('#/:db/:docid', app.showDocument);
 })
 
