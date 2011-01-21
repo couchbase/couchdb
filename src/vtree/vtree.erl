79ac2baf11766cb0cbb1cdb3d119e834508d38e6
Volker Mische <volker.mische@gmail.com>
2011-01-21 14:16:38 +0100

    Fixed bug in delete/4 when bounding box was too similar.

--- src/vtree/vtree.erl
+++ src/vtree/vtree.erl
@@ -679,7 +679,8 @@ delete(Fd, DeleteId, DeleteMbr, [NodePos|NodePosTail]) ->
                     {ok, NodeNewPos, NodePos}
                 end;
             false ->
-                not_found
+                % not found, go on with sibling nodes
+                delete(Fd, DeleteId, DeleteMbr, NodePosTail)
             end
         end;
     false ->
