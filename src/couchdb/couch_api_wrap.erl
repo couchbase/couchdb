couchbase/couchdb

commit 7c92051931429ac12841b16382c7a8fbb616e380

Improve error logging on replication write failures
If an error happens when writing a document to the target endpoint,
log it's ID, revision, error and error reason. Also added a few tests
to cover the cases where the target has validate_doc_update functions.

Change-Id: I132e20240c1e31943d97691cdd4a97d82742b22a
git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1129906 13f79535-47bb-0310-9956-ffa450edef68
Reviewed-on: http://review.couchbase.org/6709
Reviewed-by: Filipe David Borba Manana <fdmanana@gmail.com>
Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>
Tested-by: Jan Lehnardt <jan@apache.org>
Reviewed-by: Jan Lehnardt <jan@apache.org>

src/couchdb/couch_api_wrap.erl
@@ -236,6 +236,8 @@ update_doc(#httpdb{} = HttpDb, #doc{id = DocId} = Doc, Options, Type) ->
                 case {Code, get_value(<<"error">>, Props)} of
                 {401, <<"unauthorized">>} ->
                     throw({unauthorized, get_value(<<"reason">>, Props)});
+                {403, <<"forbidden">>} ->
+                    throw({forbidden, get_value(<<"reason">>, Props)});
                 {412, <<"missing_stub">>} ->
                     throw({missing_stub, get_value(<<"reason">>, Props)});
                 {_, Error} ->
@@ -665,9 +667,10 @@ bulk_results_to_errors(Docs, {ok, Results}, interactive_edit) ->
     lists:reverse(lists:foldl(
         fun({_, {ok, _}}, Acc) ->
             Acc;
-        ({#doc{id = Id}, Error}, Acc) ->
-            {_, Error, _Reason} = couch_httpd:error_info(Error),
-            [ {[{<<"id">>, Id}, {<<"error">>, Error}]} | Acc ]
+        ({#doc{id = Id, revs = {Pos, [RevId | _]}}, Error}, Acc) ->
+            {_, Error, Reason} = couch_httpd:error_info(Error),
+            [ {[{id, Id}, {rev, rev_to_str({Pos, RevId})},
+                {error, Error}, {reason, Reason}]} | Acc ]
         end,
         [], lists:zip(Docs, Results)));
 
@@ -676,9 +679,9 @@ bulk_results_to_errors(Docs, {ok, Results}, replicated_changes) ->
 
 bulk_results_to_errors(_Docs, {aborted, Results}, interactive_edit) ->
     lists:map(
-        fun({{Id, _Rev}, Err}) ->
-            {_, Error, _Reason} = couch_httpd:error_info(Err),
-            {[{<<"id">>, Id}, {<<"error">>, Error}]}
+        fun({{Id, Rev}, Err}) ->
+            {_, Error, Reason} = couch_httpd:error_info(Err),
+            {[{id, Id}, {rev, rev_to_str(Rev)}, {error, Error}, {reason, Reason}]}
         end,
         Results);
 
@@ -690,10 +693,19 @@ bulk_results_to_errors(_Docs, Results, remote) ->
                 Acc;
             Error ->
                 Id = get_value(<<"id">>, Props, get_value(id, Props)),
-                [ {[{<<"id">>, Id}, {<<"error">>, Error}]} | Acc ]
+                Rev = get_value(<<"rev">>, Props, get_value(rev, Props)),
+                Reason = get_value(<<"reason">>, Props, get_value(reason, Props)),
+                [ {[{id, Id}, {rev, rev_to_str(Rev)},
+                    {error, Error}, {reason, Reason}]} | Acc ]
             end
         end,
         [], Results)).
+
+
+rev_to_str({_Pos, _Id} = Rev) ->
+    couch_doc:rev_to_str(Rev);
+rev_to_str(Rev) ->
+    Rev.
 
 
 stream_doc({JsonBytes, Atts, Boundary, Len}) ->

src/couchdb/couch_replicator_doc_copier.erl
@@ -36,6 +36,12 @@
     start_db_compaction_notifier/2,
     stop_db_compaction_notifier/1
 ]).
+-import(couch_util, [
+    to_binary/1,
+    get_value/2,
+    get_value/3
+]).
+
 
 -record(batch, {
     docs = [],
@@ -490,15 +496,15 @@ flush_docs(Target, DocList) ->
         Target, DocList, [delay_commit], replicated_changes),
     DbUri = couch_api_wrap:db_uri(Target),
     lists:foreach(
-        fun({[ {<<"id">>, Id}, {<<"error">>, <<"unauthorized">>} ]}) ->
-                ?LOG_ERROR("Replicator: unauthorized to write document"
-                    " `~s` to `~s`", [Id, DbUri]);
-            (_) ->
-                ok
+        fun({Props}) ->
+            ?LOG_ERROR("Replicator: couldn't write document `~s`, revision `~s`,"
+                " to target database `~s`. Error: `~s`, reason: `~s`.",
+                [get_value(id, Props, ""), get_value(rev, Props, ""), DbUri,
+                    get_value(error, Props, ""), get_value(reason, Props, "")])
         end, Errors),
     {length(DocList) - length(Errors), length(Errors)}.
 
-flush_doc(Target, #doc{id = Id} = Doc) ->
+flush_doc(Target, #doc{id = Id, revs = {Pos, [RevId | _]}} = Doc) ->
     try couch_api_wrap:update_doc(Target, Doc, [], replicated_changes) of
     {ok, _} ->
         ok;
@@ -507,8 +513,16 @@ flush_doc(Target, #doc{id = Id} = Doc) ->
             [Id, couch_api_wrap:db_uri(Target), couch_util:to_binary(Error)]),
         Error
     catch
-    throw:{unauthorized, _} ->
-        ?LOG_ERROR("Replicator: unauthorized to write document `~s` to `~s`",
-            [Id, couch_api_wrap:db_uri(Target)]),
-        {error, unauthorized}
+    throw:{Error, Reason} ->
+        ?LOG_ERROR("Replicator: couldn't write document `~s`, revision `~s`,"
+            " to target database `~s`. Error: `~s`, reason: `~s`.",
+            [Id, couch_doc:rev_to_str({Pos, RevId}),
+                couch_api_wrap:db_uri(Target), to_binary(Error), to_binary(Reason)]),
+        {error, Error};
+    Tag:Err ->
+        ?LOG_ERROR("Replicator: couldn't write document `~s`, revision `~s`,"
+            " to target database `~s`. Error: `~s`.",
+            [Id, couch_doc:rev_to_str({Pos, RevId}),
+                couch_api_wrap:db_uri(Target), to_binary({Tag, Err})]),
+        {error, Err}
     end.

share/www/script/test/replication.js
@@ -504,6 +504,53 @@ couchTests.replication = function(debug) {
   }
 
 
+  // test errors due to doc validate_doc_update functions in the target endpoint
+  docs = makeDocs(1, 8);
+  docs[2]["_attachments"] = {
+    "hello.txt": {
+      "content_type": "text/plain",
+      "data": "aGVsbG8gd29ybGQ="  // base64:encode("hello world")
+    }
+  };
+  var ddoc = {
+    _id: "_design/test",
+    language: "javascript",
+    validate_doc_update: (function(newDoc, oldDoc, userCtx, secObj) {
+      if ((newDoc.integer % 2) !== 0) {
+        throw {forbidden: "I only like multiples of 2."};
+      }
+    }).toString()
+  };
+
+  for (i = 0; i < dbPairs.length; i++) {
+    populateDb(sourceDb, docs);
+    populateDb(targetDb, [ddoc]);
+
+    repResult = CouchDB.replicate(
+      dbPairs[i].source,
+      dbPairs[i].target
+    );
+    TEquals(true, repResult.ok);
+    TEquals(7, repResult.history[0].missing_checked);
+    TEquals(7, repResult.history[0].missing_found);
+    TEquals(7, repResult.history[0].docs_read);
+    TEquals(3, repResult.history[0].docs_written);
+    TEquals(4, repResult.history[0].doc_write_failures);
+
+    for (j = 0; j < docs.length; j++) {
+      doc = docs[j];
+      copy = targetDb.open(doc._id);
+
+      if (doc.integer % 2 === 0) {
+        T(copy !== null);
+        TEquals(copy.integer, doc.integer);
+      } else {
+        T(copy === null);
+      }
+    }
+  }
+
+
   // test create_target option
   docs = makeDocs(1, 2);
 

commit ad483c7bdf538888b123e03aae7d95ce61e92599

Fix replicator multipart doc fetch retrying process
When a document has attachments and a connection error happens
when streaming them, the multipart parser throws an exception
to signal that a document fetch retry must be performed. These
exceptions were being caught by the callback preventing the
document fetch operation from being retried.

This fixes a regression introduced in revision:

    7c92051931429ac12841b16382c7a8fbb616e380

It also improves the process mailbox cleanup to make sure we don't
reuse data received from a previous attempt.

Change-Id: If425faa9a3e1e9adc8f9e1f64a21c6b3389e4099
Reviewed-on: http://review.couchbase.org/7142
Reviewed-by: Filipe David Borba Manana <fdmanana@gmail.com>
Reviewed-by: Dustin Sallings <dustin@spy.net>
Tested-by: Dustin Sallings <dustin@spy.net>

src/couchdb/couch_api_wrap.erl
@@ -174,8 +174,8 @@ open_doc_revs(#httpdb{} = HttpDb, Id, Revs, Options, Fun, Acc) ->
             unlink(Self)
         end),
     receive
-    started ->
-        receive_docs_loop(Streamer, Fun, Acc)
+    {started_open_doc_revs, Ref} ->
+        receive_docs_loop(Streamer, Fun, Id, Revs, Ref, Acc)
     end;
 open_doc_revs(Db, Id, Revs, Options, Fun, Acc) ->
     {ok, Results} = couch_db:open_doc_revs(Db, Id, Revs, Options),
@@ -459,24 +459,28 @@ atts_since_arg(UrlLen, [PA | Rest], Acc) ->
 %       the exported open_doc_revs/6 function. The restart should be
 %       transparent to the caller like any other Couch API function exported
 %       by this module.
-receive_docs_loop(Streamer, Fun, Acc) ->
+receive_docs_loop(Streamer, Fun, Id, Revs, Ref, Acc) ->
     try
-        receive_docs(Streamer, Fun, Acc)
+        % Left only for debugging purposes via an interactive or remote shell
+        erlang:put(open_doc_revs, {Id, Revs, Ref, Streamer}),
+        receive_docs(Streamer, Fun, Ref, Acc)
     catch
-    throw:restart ->
-        receive_docs_loop(Streamer, Fun, Acc)
+    error:{restart_open_doc_revs, NewRef} ->
+        receive_docs_loop(Streamer, Fun, Id, Revs, NewRef, Acc)
     end.
 
-receive_docs(Streamer, UserFun, UserAcc) ->
-    Streamer ! {get_headers, self()},
+receive_docs(Streamer, UserFun, Ref, UserAcc) ->
+    Streamer ! {get_headers, Ref, self()},
     receive
-    started ->
-        restart_remote_open_doc_revs();
-    {headers, Headers} ->
+    {started_open_doc_revs, NewRef} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {headers, Ref, Headers} ->
         case get_value("content-type", Headers) of
         {"multipart/related", _} = ContentType ->
             case doc_from_multi_part_stream(
-                ContentType, fun() -> receive_doc_data(Streamer) end) of
+                ContentType,
+                fun() -> receive_doc_data(Streamer, Ref) end,
+                Ref) of
             {ok, Doc, Parser} ->
                 case UserFun({ok, Doc}, UserAcc) of
                 {ok, UserAcc2} ->
@@ -484,98 +488,96 @@ receive_docs(Streamer, UserFun, UserAcc) ->
                 {skip, UserAcc2} ->
                     couch_doc:abort_multi_part_stream(Parser)
                 end,
-                receive_docs(Streamer, UserFun, UserAcc2)
+                receive_docs(Streamer, UserFun, Ref, UserAcc2)
             end;
         {"application/json", []} ->
             Doc = couch_doc:from_json_obj(
-                    ?JSON_DECODE(receive_all(Streamer, []))),
+                    ?JSON_DECODE(receive_all(Streamer, Ref, []))),
             {_, UserAcc2} = UserFun({ok, Doc}, UserAcc),
-            receive_docs(Streamer, UserFun, UserAcc2);
+            receive_docs(Streamer, UserFun, Ref, UserAcc2);
         {"application/json", [{"error","true"}]} ->
-            {ErrorProps} = ?JSON_DECODE(receive_all(Streamer, [])),
+            {ErrorProps} = ?JSON_DECODE(receive_all(Streamer, Ref, [])),
             Rev = get_value(<<"missing">>, ErrorProps),
             Result = {{not_found, missing}, couch_doc:parse_rev(Rev)},
             {_, UserAcc2} = UserFun(Result, UserAcc),
-            receive_docs(Streamer, UserFun, UserAcc2)
+            receive_docs(Streamer, UserFun, Ref, UserAcc2)
         end;
-    done ->
+    {done, Ref} ->
         {ok, UserAcc}
     end.
 
 
-restart_remote_open_doc_revs() ->
+restart_remote_open_doc_revs(Ref, NewRef) ->
     receive
-    {body_bytes, _} ->
-        restart_remote_open_doc_revs();
-    body_done ->
-        restart_remote_open_doc_revs();
-    done ->
-        restart_remote_open_doc_revs();
-    {headers, _} ->
-        restart_remote_open_doc_revs();
-    started ->
-        restart_remote_open_doc_revs()
+    {body_bytes, Ref, _} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {body_done, Ref} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {done, Ref} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {headers, Ref, _} ->
+        restart_remote_open_doc_revs(Ref, NewRef)
     after 0 ->
-        throw(restart)
+        erlang:error({restart_open_doc_revs, NewRef})
     end.
 
 
 remote_open_doc_revs_streamer_start(Parent) ->
     receive
-    {get_headers, _} ->
+    {get_headers, _Ref, Parent} ->
         remote_open_doc_revs_streamer_start(Parent);
-    {next_bytes, _} ->
+    {next_bytes, _Ref, Parent} ->
         remote_open_doc_revs_streamer_start(Parent)
     after 0 ->
-        Parent ! started
+        Parent ! {started_open_doc_revs, make_ref()}
     end.
 
 
-receive_all(Streamer, Acc) ->
-    Streamer ! {next_bytes, self()},
+receive_all(Streamer, Ref, Acc) ->
+    Streamer ! {next_bytes, Ref, self()},
     receive
-    started ->
-        restart_remote_open_doc_revs();
-    {body_bytes, Bytes} ->
-        receive_all(Streamer, [Bytes | Acc]);
-    body_done ->
+    {started_open_doc_revs, NewRef} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {body_bytes, Ref, Bytes} ->
+        receive_all(Streamer, Ref, [Bytes | Acc]);
+    {body_done, Ref} ->
         lists:reverse(Acc)
     end.
 
 
 mp_parse_mixed(eof) ->
-    receive {get_headers, From} ->
-        From ! done
+    receive {get_headers, Ref, From} ->
+        From ! {done, Ref}
     end;
 mp_parse_mixed({headers, H}) ->
-    receive {get_headers, From} ->
-        From ! {headers, H}
+    receive {get_headers, Ref, From} ->
+        From ! {headers, Ref, H}
     end,
     fun mp_parse_mixed/1;
 mp_parse_mixed({body, Bytes}) ->
-    receive {next_bytes, From} ->
-        From ! {body_bytes, Bytes}
+    receive {next_bytes, Ref, From} ->
+        From ! {body_bytes, Ref, Bytes}
     end,
     fun mp_parse_mixed/1;
 mp_parse_mixed(body_end) ->
-    receive {next_bytes, From} ->
-        From ! body_done;
-    {get_headers, From} ->
-        self() ! {get_headers, From}
+    receive {next_bytes, Ref, From} ->
+        From ! {body_done, Ref};
+    {get_headers, Ref, From} ->
+        self() ! {get_headers, Ref, From}
     end,
     fun mp_parse_mixed/1.
 
 
-receive_doc_data(Streamer) ->
-    Streamer ! {next_bytes, self()},
+receive_doc_data(Streamer, Ref) ->
+    Streamer ! {next_bytes, Ref, self()},
     receive
-    {body_bytes, Bytes} ->
-        {Bytes, fun() -> receive_doc_data(Streamer) end};
-    body_done ->
-        {<<>>, fun() -> receive_doc_data(Streamer) end}
+    {body_bytes, Ref, Bytes} ->
+        {Bytes, fun() -> receive_doc_data(Streamer, Ref) end};
+    {body_done, Ref} ->
+        {<<>>, fun() -> receive_doc_data(Streamer, Ref) end}
     end.
 
-doc_from_multi_part_stream(ContentType, DataFun) ->
+doc_from_multi_part_stream(ContentType, DataFun, Ref) ->
     Self = self(),
     Parser = spawn_link(fun() ->
         {<<"--">>, _, _} = couch_httpd:parse_multipart_request(
@@ -583,22 +585,23 @@ doc_from_multi_part_stream(ContentType, DataFun) ->
             fun(Next) -> couch_doc:mp_parse_doc(Next, []) end),
         unlink(Self)
         end),
-    Parser ! {get_doc_bytes, self()},
+    Parser ! {get_doc_bytes, Ref, self()},
     receive
-    started ->
+    {started_open_doc_revs, NewRef} ->
         unlink(Parser),
         exit(Parser, kill),
-        restart_remote_open_doc_revs();
-    {doc_bytes, DocBytes} ->
+        restart_remote_open_doc_revs(Ref, NewRef);
+    {doc_bytes, Ref, DocBytes} ->
         Doc = couch_doc:from_json_obj(?JSON_DECODE(DocBytes)),
         ReadAttachmentDataFun = fun() ->
-            Parser ! {get_bytes, self()},
+            Parser ! {get_bytes, Ref, self()},
             receive
-            started ->
+            {started_open_doc_revs, NewRef} ->
                 unlink(Parser),
                 exit(Parser, kill),
-                restart_remote_open_doc_revs();
-            {bytes, Bytes} ->
+                receive {bytes, Ref, _} -> ok after 0 -> ok end,
+                restart_remote_open_doc_revs(Ref, NewRef);
+            {bytes, Ref, Bytes} ->
                 Bytes
             end
         end,
@@ -721,8 +724,8 @@ stream_doc({JsonBytes, Atts, Boundary, Len}) ->
         couch_doc:doc_to_multi_part_stream(
             Boundary, JsonBytes, Atts,
             fun(Data) ->
-                receive {get_data, From} ->
-                    From ! {data, Data}
+                receive {get_data, Ref, From} ->
+                    From ! {data, Ref, Data}
                 end
             end, true),
         unlink(Self)
@@ -733,7 +736,8 @@ stream_doc({0, Id}) ->
     erlang:erase({doc_streamer, Id}),
     eof;
 stream_doc({LenLeft, Id}) when LenLeft > 0 ->
-    erlang:get({doc_streamer, Id}) ! {get_data, self()},
-    receive {data, Data} ->
+    Ref = make_ref(),
+    erlang:get({doc_streamer, Id}) ! {get_data, Ref, self()},
+    receive {data, Ref, Data} ->
         {ok, Data, {LenLeft - iolist_size(Data), Id}}
     end.

src/couchdb/couch_doc.erl
@@ -490,15 +490,16 @@ doc_from_multi_part_stream(ContentType, DataFun) ->
         unlink(Parent),
         Parent ! {self(), finished}
         end),
-    Parser ! {get_doc_bytes, self()},
+    Ref = make_ref(),
+    Parser ! {get_doc_bytes, Ref, self()},
     receive 
-    {doc_bytes, DocBytes} ->
+    {doc_bytes, Ref, DocBytes} ->
         Doc = from_json_obj(?JSON_DECODE(DocBytes)),
         % go through the attachments looking for 'follows' in the data,
         % replace with function that reads the data from MIME stream.
         ReadAttachmentDataFun = fun() ->
-            Parser ! {get_bytes, self()},
-            receive {bytes, Bytes} -> Bytes end
+            Parser ! {get_bytes, Ref, self()},
+            receive {bytes, Ref, Bytes} -> Bytes end
         end,
         Atts2 = lists:map(
             fun(#att{data=follows}=A) ->
@@ -525,8 +526,8 @@ mp_parse_doc({body, Bytes}, AccBytes) ->
         mp_parse_doc(Next, [Bytes | AccBytes])
     end;
 mp_parse_doc(body_end, AccBytes) ->
-    receive {get_doc_bytes, From} ->
-        From ! {doc_bytes, lists:reverse(AccBytes)}
+    receive {get_doc_bytes, Ref, From} ->
+        From ! {doc_bytes, Ref, lists:reverse(AccBytes)}
     end,
     fun mp_parse_atts/1.
 
@@ -535,8 +536,8 @@ mp_parse_atts(eof) ->
 mp_parse_atts({headers, _H}) ->
     fun mp_parse_atts/1;
 mp_parse_atts({body, Bytes}) ->
-    receive {get_bytes, From} ->
-        From ! {bytes, Bytes}
+    receive {get_bytes, Ref, From} ->
+        From ! {bytes, Ref, Bytes}
     end,
     fun mp_parse_atts/1;
 mp_parse_atts(body_end) ->
@@ -549,9 +550,9 @@ abort_multi_part_stream(Parser) ->
 abort_multi_part_stream(Parser, MonRef) ->
     case is_process_alive(Parser) of
     true ->
-        Parser ! {get_bytes, self()},
+        Parser ! {get_bytes, nil, self()},
         receive
-        {bytes, _Bytes} ->
+        {bytes, nil, _Bytes} ->
              abort_multi_part_stream(Parser, MonRef);
         {'DOWN', MonRef, _, _, _} ->
              ok

src/couchdb/couch_replicator_doc_copier.erl
@@ -521,10 +521,10 @@ flush_doc(Target, #doc{id = Id, revs = {Pos, [RevId | _]}} = Doc) ->
             [Id, couch_doc:rev_to_str({Pos, RevId}),
                 couch_api_wrap:db_uri(Target), to_binary(Error), to_binary(Reason)]),
         {error, Error};
-    Tag:Err ->
+    throw:Err ->
         ?LOG_ERROR("Replicator: couldn't write document `~s`, revision `~s`,"
             " to target database `~s`. Error: `~s`.",
             [Id, couch_doc:rev_to_str({Pos, RevId}),
-                couch_api_wrap:db_uri(Target), to_binary({Tag, Err})]),
+                couch_api_wrap:db_uri(Target), to_binary(Err)]),
         {error, Err}
     end.