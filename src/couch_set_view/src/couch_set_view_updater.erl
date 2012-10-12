4a1882b75b364d959b82100fe7971afc98fc322b
Filipe David Borba Manana <fdmanana@apache.org>
2012-10-12 14:02:57 +0100

    Revert "MB-6629 Faster index compaction retries"
    This reverts commit 719e315b3713a91fde0070ed5b2b552afc5aa44e.
    
    Change-Id: I48a351f929d37121682d7303cdaaf530ede59221
    Reviewed-on: http://review.couchbase.org/21582
    Reviewed-by: Farshid Ghods <farshid@couchbase.com>
    Tested-by: Farshid Ghods <farshid@couchbase.com>

--- src/couch_set_view/src/couch_set_view_updater.erl
+++ src/couch_set_view/src/couch_set_view_updater.erl
@@ -825,6 +825,7 @@ view_insert_doc_query_results(DocId, PartitionId, [ResultKVs | RestResults],
 write_changes(WriterAcc, ViewKeyValuesToAdd, DocIdViewIdKeys, PartIdSeqs) ->
     #writer_acc{
         log_fd = LogFd,
+        owner = Owner,
         group = Group,
         initial_build = false,
         cleanup_kv_count = CleanupKvCount0,
@@ -933,7 +934,9 @@ write_changes(WriterAcc, ViewKeyValuesToAdd, DocIdViewIdKeys, PartIdSeqs) ->
             ViewKeyValuesToAddBinary),
         LogEntry = {NewSeqs, AddDocIdViewIdKeys, RemoveDocIds, LogViewsAddRemoveKvs},
         LogEntryBin = couch_compress:compress(?term_to_bin(LogEntry)),
-        ok = file:write(LogFd, [<<(byte_size(LogEntryBin)):32>>, LogEntryBin])
+        ok = file:write(LogFd, [<<(byte_size(LogEntryBin)):32>>, LogEntryBin]),
+        {ok, LogEof} = file:position(LogFd, eof),
+        ok = gen_server:cast(Owner, {log_eof, LogEof})
     end,
     Header = Group#set_view_group.index_header,
     NewHeader = Header#set_view_index_header{
