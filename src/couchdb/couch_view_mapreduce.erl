couchbase/couchdb

commit 912b8667560a275a652a2ff0d9d9264ed38e9328

MB-7978 Fixes to view tree with multiple reducers
A view tree with multiple reducers where not all of them
are native (_count, _sum, _stats) or custom (JavaScript)
was not working anymore since the termless storage format,
and it became undetected for so long due to the fact that
old JavaScript testsuite (Futon) was removed (commit
304cbbd93400abfcb07ca70eecb4e1ccf3f3b485) and no equivalent
etap tests existed.

A view tree with multiple reducers only exists when 2 or
more views in the same design document have exactly the same
map function, on a byte by byte basis, and different reduce
functions (builtin or custom).

Change-Id: Id3218842da83e9e26339e6c59dd7c9b2e8d6a999
Reviewed-on: http://review.couchbase.org/25390
Reviewed-by: Volker Mische <volker.mische@gmail.com>
Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>

src/couchdb/couch_view_mapreduce.erl
@@ -84,11 +84,11 @@ reduce(#view{ref = Ref, reduce_funs = RedFuns}, KVs0) ->
     {NativeFuns, JsFuns} = lists:partition(
         fun(<<"_", _/binary>>) -> true; (_) -> false end,
         RedFunSources),
+    KVs = encode_kvs(KVs0, []),
     case JsFuns of
     [] ->
-        builtin_reduce(reduce, NativeFuns, KVs0, []);
+        builtin_reduce(reduce, NativeFuns, KVs, []);
     _ ->
-        KVs = encode_kvs(KVs0, []),
         {ok, NativeResults} = builtin_reduce(reduce, NativeFuns, KVs, []),
         Ctx = erlang:get({reduce_context, Ref}),
         case mapreduce:reduce(Ctx, KVs) of
@@ -105,14 +105,14 @@ reduce(#view{reduce_funs = []}, _NthRed, _KVs) ->
     {ok, []};
 reduce(#view{ref = Ref, reduce_funs = RedFuns}, NthRed, KVs0) ->
     {Before, [{_Name, FunSrc} | _]} = lists:split(NthRed - 1, RedFuns),
+    KVs = encode_kvs(KVs0, []),
     case FunSrc of
     <<"_", _/binary>> ->
-        builtin_reduce(reduce, [FunSrc], KVs0, []);
+        builtin_reduce(reduce, [FunSrc], KVs, []);
     _ ->
-        KVs = encode_kvs(KVs0, []),
         Ctx = erlang:get({reduce_context, Ref}),
         NthRed2 = lists:foldl(
-            fun(<<"_", _/binary>>, Acc) ->
+            fun({_, <<"_", _/binary>>}, Acc) ->
                     Acc - 1;
                 (_, Acc) ->
                     Acc
@@ -159,7 +159,7 @@ rereduce(#view{ref = Ref, reduce_funs = RedFuns}, NthRed, ReducedValues) ->
     _ ->
         Ctx = erlang:get({reduce_context, Ref}),
         NthRed2 = lists:foldl(
-            fun(<<"_", _/binary>>, Acc) ->
+            fun({_, <<"_", _/binary>>}, Acc) ->
                     Acc - 1;
                 (_, Acc) ->
                     Acc
@@ -213,13 +213,7 @@ group_reductions_results(List) ->
 builtin_reduce(_Re, [], _KVs, Acc) ->
     {ok, lists:reverse(Acc)};
 builtin_reduce(Re, [<<"_sum", _/binary>> | BuiltinReds], KVs, Acc) ->
-    case Re of
-    reduce ->
-        KVs2 = contract_kvs(KVs, []);
-    rereduce ->
-        KVs2 = KVs
-    end,
-    Sum = builtin_sum_rows(KVs2),
+    Sum = builtin_sum_rows(KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Sum | Acc]);
 builtin_reduce(reduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
     Count = length(KVs),
@@ -228,13 +222,7 @@ builtin_reduce(rereduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
     Count = builtin_sum_rows(KVs),
     builtin_reduce(rereduce, BuiltinReds, KVs, [Count | Acc]);
 builtin_reduce(Re, [<<"_stats", _/binary>> | BuiltinReds], KVs, Acc) ->
-    case Re of
-    reduce ->
-        KVs2 = contract_kvs(KVs, []);
-    rereduce ->
-        KVs2 = KVs
-    end,
-    Stats = builtin_stats(Re, KVs2),
+    Stats = builtin_stats(Re, KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Stats | Acc]);
 builtin_reduce(_Re, [InvalidBuiltin | _BuiltinReds], _KVs, _Acc) ->
     throw({error, <<"Invalid builtin reduce function: ", InvalidBuiltin/binary>>}).
@@ -287,13 +275,6 @@ builtin_stats(rereduce, [{_, First} | Rest]) ->
     {Sum, Cnt, Min, Max, Sqr} = Stats,
     {[{<<"sum">>, Sum}, {<<"count">>, Cnt}, {<<"min">>, Min}, {<<"max">>, Max}, {<<"sumsqr">>, Sqr}]}.
 
-
-contract_kvs([], Acc) ->
-    lists:reverse(Acc);
-contract_kvs([KV | Rest], Acc) ->
-    {{Key, Id}, Value} = KV,
-    NKV = {[Key, Id], Value},
-    contract_kvs(Rest, [NKV | Acc]).
 
 encode_kvs([], Acc) ->
     lists:reverse(Acc);

src/couch_set_view/Makefile.am
@@ -63,7 +63,8 @@ test_files = \
     test/22-compactor-cleanup.t \
     test/23-replica-group-missing.t \
     test/24-updater-add-more-passive-partitions.t \
-    test/25-util-stats.t
+    test/25-util-stats.t \
+    test/26-multiple-reductions.t
 
 compiled_files = \
     ebin/couch_set_view.app \

src/couch_set_view/src/couch_set_view_mapreduce.erl
@@ -83,11 +83,11 @@ reduce(#set_view{ref = Ref, reduce_funs = RedFuns}, KVs0) ->
     {NativeFuns, JsFuns} = lists:partition(
         fun(<<"_", _/binary>>) -> true; (_) -> false end,
         RedFunSources),
+    KVs = encode_kvs(KVs0, []),
     case JsFuns of
     [] ->
-        builtin_reduce(reduce, NativeFuns, KVs0, []);
+        builtin_reduce(reduce, NativeFuns, KVs, []);
     _ ->
-        KVs = encode_kvs(KVs0, []),
         {ok, NativeResults} = builtin_reduce(reduce, NativeFuns, KVs, []),
         Ctx = erlang:get({reduce_context, Ref}),
         case mapreduce:reduce(Ctx, KVs) of
@@ -103,14 +103,14 @@ reduce(#set_view{reduce_funs = []}, _NthRed, _KVs) ->
     {ok, []};
 reduce(#set_view{ref = Ref, reduce_funs = RedFuns}, NthRed, KVs0) ->
     {Before, [{_Name, FunSrc} | _]} = lists:split(NthRed - 1, RedFuns),
+    KVs = encode_kvs(KVs0, []),
     case FunSrc of
     <<"_", _/binary>> ->
-        builtin_reduce(reduce, [FunSrc], KVs0, []);
+        builtin_reduce(reduce, [FunSrc], KVs, []);
     _ ->
-        KVs = encode_kvs(KVs0, []),
         Ctx = erlang:get({reduce_context, Ref}),
         NthRed2 = lists:foldl(
-            fun(<<"_", _/binary>>, Acc) ->
+            fun({_, <<"_", _/binary>>}, Acc) ->
                     Acc - 1;
                 (_, Acc) ->
                     Acc
@@ -157,7 +157,7 @@ rereduce(#set_view{ref = Ref, reduce_funs = RedFuns}, NthRed, ReducedValues) ->
     _ ->
         Ctx = erlang:get({reduce_context, Ref}),
         NthRed2 = lists:foldl(
-            fun(<<"_", _/binary>>, Acc) ->
+            fun({_, <<"_", _/binary>>}, Acc) ->
                     Acc - 1;
                 (_, Acc) ->
                     Acc
@@ -181,7 +181,7 @@ reduce_fun_indexes(RedFuns) ->
                 {Idx, Idx + 1}
         end,
         1, RedFuns),
-    lists:reverse(L).
+    L.
 
 
 recombine_reduce_results([], [], [], Acc) ->
@@ -214,13 +214,7 @@ builtin_reduce(ReduceType, FunSrcs, Values) ->
 builtin_reduce(_Re, [], _KVs, Acc) ->
     {ok, lists:reverse(Acc)};
 builtin_reduce(Re, [<<"_sum", _/binary>> | BuiltinReds], KVs, Acc) ->
-    case Re of
-    reduce ->
-        KVs2 = contract_kvs(KVs, []);
-    rereduce ->
-        KVs2 = KVs
-    end,
-    Sum = builtin_sum_rows(KVs2),
+    Sum = builtin_sum_rows(KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Sum | Acc]);
 builtin_reduce(reduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
     Json = ?JSON_ENCODE(length(KVs)),
@@ -229,13 +223,7 @@ builtin_reduce(rereduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
     Count = builtin_sum_rows(KVs),
     builtin_reduce(rereduce, BuiltinReds, KVs, [Count | Acc]);
 builtin_reduce(Re, [<<"_stats", _/binary>> | BuiltinReds], KVs, Acc) ->
-    case Re of
-    reduce ->
-        KVs2 = contract_kvs(KVs, []);
-    rereduce ->
-        KVs2 = KVs
-    end,
-    Stats = builtin_stats(Re, KVs2),
+    Stats = builtin_stats(Re, KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Stats | Acc]);
 builtin_reduce(_Re, [InvalidBuiltin | _BuiltinReds], _KVs, _Acc) ->
     throw({error, <<"Invalid builtin reduce function: ", InvalidBuiltin/binary>>}).
@@ -304,13 +292,6 @@ builtin_stats(rereduce, [{_, First} | Rest]) ->
         {<<"sumsqr">>, Sqr}
     ]},
     ?JSON_ENCODE(Result).
-
-
-contract_kvs([], Acc) ->
-    lists:reverse(Acc);
-contract_kvs([KV | Rest], Acc) ->
-    {KeyId, <<_PartId:16, Value/binary>>} = KV,
-    contract_kvs(Rest, [{KeyId, Value} | Acc]).
 
 
 encode_kvs([], Acc) ->

src/couch_set_view/test/26-multiple-reductions.t
@@ -1,0 +1,380 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -smp enable
+
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-include_lib("couch_set_view/include/couch_set_view.hrl").
+
+-define(MAX_WAIT_TIME, 900 * 1000).
+
+-define(etap_match(Got, Expected, Desc),
+        etap:fun_is(fun(XXXXXX) ->
+            case XXXXXX of Expected -> true; _ -> false end
+        end, Got, Desc)).
+
+test_set_name() -> <<"couch_test_set_index_multiple_reductions">>.
+num_set_partitions() -> 64.
+ddoc_id() -> <<"_design/test">>.
+initial_num_docs() -> 70400.  % must be multiple of num_set_partitions()
+
+
+% from couch_db.hrl
+-define(MIN_STR, <<>>).
+-define(MAX_STR, <<255>>).
+
+-record(view_query_args, {
+    start_key,
+    end_key,
+    start_docid = ?MIN_STR,
+    end_docid = ?MAX_STR,
+    direction = fwd,
+    inclusive_end = true,
+    limit = 10000000000,
+    skip = 0,
+    group_level = 0,
+    view_type = nil,
+    include_docs = false,
+    conflicts = false,
+    stale = false,
+    multi_get = false,
+    callback = nil,
+    list = nil,
+    run_reduce = true,
+    keys = nil,
+    view_name = nil,
+    debug = false,
+    filter = true,
+    type = main
+}).
+
+
+main(_) ->
+    test_util:init_code_path(),
+
+    etap:plan(34),
+    case (catch test()) of
+        ok ->
+            etap:end_tests();
+        Other ->
+            etap:diag(io_lib:format("Test died abnormally: ~p", [Other])),
+            etap:bail(Other)
+    end,
+    ok.
+
+
+test() ->
+    couch_set_view_test_util:start_server(test_set_name()),
+
+    ValueGenFun1 = fun(I) -> I end,
+    create_set(),
+    update_documents(0, initial_num_docs(), ValueGenFun1),
+    trigger_update(),
+
+    QueryResult1 = query_reduce_view(<<"test2">>, doc_id(1000), doc_id(5000)),
+    etap:is(QueryResult1, <<"4001">>, "Query result is \"4001\""),
+
+    QueryResult2 = query_reduce_view(<<"test5">>, [doc_id(1000),1], [doc_id(5000),1]),
+    etap:is(QueryResult2, 4001, "Query result is 4001"),
+
+    QueryResult3 = query_reduce_view(<<"test2">>, doc_id(5000), doc_id(15000)),
+    etap:is(QueryResult3, <<"10001">>, "Query result is \"10001\""),
+
+    QueryResult4 = query_reduce_view(<<"test4">>, [doc_id(2000),1], [doc_id(3500),1]),
+    etap:is(QueryResult4, -1501, "Query result is -1501"),
+
+    verify_btrees_1(ValueGenFun1),
+    compact_view_group(),
+    verify_btrees_1(ValueGenFun1),
+
+    QueryResult11 = query_reduce_view(<<"test2">>, doc_id(1000), doc_id(5000)),
+    etap:is(QueryResult11, <<"4001">>, "Query result is \"4001\""),
+
+    QueryResult22 = query_reduce_view(<<"test5">>, [doc_id(1000),1], [doc_id(5000),1]),
+    etap:is(QueryResult22, 4001, "Query result is 4001"),
+
+    QueryResult33 = query_reduce_view(<<"test2">>, doc_id(5000), doc_id(15000)),
+    etap:is(QueryResult33, <<"10001">>, "Query result is \"10001\""),
+
+    QueryResult44 = query_reduce_view(<<"test4">>, [doc_id(2000),1], [doc_id(3500),1]),
+    etap:is(QueryResult44, -1501, "Query result is -1501"),
+
+    couch_set_view_test_util:delete_set_dbs(test_set_name(), num_set_partitions()),
+    ok = timer:sleep(1000),
+    couch_set_view_test_util:stop_server(),
+    ok.
+
+
+create_set() ->
+    couch_set_view_test_util:delete_set_dbs(test_set_name(), num_set_partitions()),
+    couch_set_view_test_util:create_set_dbs(test_set_name(), num_set_partitions()),
+    couch_set_view:cleanup_index_files(test_set_name()),
+    etap:diag("Creating the set databases (# of partitions: " ++
+        integer_to_list(num_set_partitions()) ++ ")"),
+    DDoc = {[
+        {<<"meta">>, {[{<<"id">>, ddoc_id()}]}},
+        {<<"json">>, {[
+        {<<"language">>, <<"javascript">>},
+        {<<"views">>, {[
+            {<<"test">>, {[
+                {<<"map">>, <<"function(doc, meta) { emit(meta.id, doc.value); }">>},
+                {<<"reduce">>, <<"_count">>}
+            ]}},
+            {<<"test2">>, {[
+                {<<"map">>, <<"function(doc, meta) { emit(meta.id, doc.value); }">>},
+                {<<"reduce">>, <<"function(key, values, rereduce) {"
+                                 "  if (rereduce) {"
+                                 "     var s = 0;"
+                                 "     for (var i = 0; i < values.length; ++i) {"
+                                 "         s += Number(values[i]);"
+                                 "     }"
+                                 "     return String(s);"
+                                 "  }"
+                                 "  return String(values.length);"
+                                 "}">>}
+            ]}},
+            {<<"test3">>, {[
+                {<<"map">>, <<"function(doc, meta) { emit([meta.id,1], doc.value); }">>},
+                {<<"reduce">>, <<"_sum">>}
+            ]}},
+            {<<"test4">>, {[
+                {<<"map">>, <<"function(doc, meta) { emit([meta.id,1], doc.value); }">>},
+                {<<"reduce">>, <<"function(key, values, rereduce) {"
+                                 "  if (rereduce) return sum(values);"
+                                 "  return -values.length;"
+                                 "}">>}
+            ]}},
+            {<<"test5">>, {[
+                {<<"map">>, <<"function(doc, meta) { emit([meta.id,1], doc.value); }">>},
+                {<<"reduce">>, <<"_count">>}
+            ]}}
+        ]}}
+        ]}}
+    ]},
+    ok = couch_set_view_test_util:update_ddoc(test_set_name(), DDoc),
+    etap:diag("Configuring set view with partitions [0 .. 63] as active"),
+    Params = #set_view_params{
+        max_partitions = num_set_partitions(),
+        active_partitions = lists:seq(0, 63),
+        passive_partitions = [],
+        use_replica_index = false
+    },
+    ok = couch_set_view:define_group(test_set_name(), ddoc_id(), Params).
+
+
+update_documents(StartId, Count, ValueGenFun) ->
+    etap:diag("Updating " ++ integer_to_list(Count) ++ " documents"),
+    DocList0 = lists:map(
+        fun(I) ->
+            {I rem num_set_partitions(), {[
+                {<<"meta">>, {[{<<"id">>, doc_id(I)}]}},
+                {<<"json">>, {[
+                    {<<"value">>, ValueGenFun(I)}
+                ]}}
+            ]}}
+        end,
+        lists:seq(StartId, StartId + Count - 1)),
+    DocList = [Doc || {_, Doc} <- lists:keysort(1, DocList0)],
+    ok = couch_set_view_test_util:populate_set_sequentially(
+        test_set_name(),
+        lists:seq(0, num_set_partitions() - 1),
+        DocList).
+
+
+doc_id(I) ->
+    iolist_to_binary(io_lib:format("doc_~8..0b", [I])).
+
+
+compact_view_group() ->
+    {ok, CompactPid} = couch_set_view_compactor:start_compact(
+        test_set_name(), ddoc_id(), main),
+    Ref = erlang:monitor(process, CompactPid),
+    etap:diag("Waiting for view group compaction to finish"),
+    receive
+    {'DOWN', Ref, process, CompactPid, normal} ->
+        ok;
+    {'DOWN', Ref, process, CompactPid, noproc} ->
+        ok;
+    {'DOWN', Ref, process, CompactPid, Reason} ->
+        etap:bail("Failure compacting main group: " ++ couch_util:to_list(Reason))
+    after ?MAX_WAIT_TIME ->
+        etap:bail("Timeout waiting for main group compaction to finish")
+    end.
+
+
+trigger_update() ->
+    GroupPid = couch_set_view:get_group_pid(test_set_name(), ddoc_id()),
+    {ok, UpdaterPid} = gen_server:call(GroupPid, {start_updater, []}, infinity),
+    Ref = erlang:monitor(process, UpdaterPid),
+    etap:diag("Waiting for updater to finish"),
+    receive
+    {'DOWN', Ref, _, _, {updater_finished, _}} ->
+        etap:diag("Updater finished");
+    {'DOWN', Ref, _, _, Reason} ->
+        etap:bail("Updater finished with unexpected reason: " ++ couch_util:to_list(Reason))
+    after ?MAX_WAIT_TIME ->
+        etap:bail("Timeout waiting for updater to finish")
+    end.
+
+
+query_reduce_view(ViewName, StartKey, EndKey) ->
+    etap:diag("Querying reduce view " ++ binary_to_list(ViewName)),
+    {ok, View, Group, _} = couch_set_view:get_reduce_view(
+        test_set_name(), ddoc_id(), ViewName,
+        #set_view_group_req{stale = false, debug = true}),
+    FoldFun = fun(Key, Red, Acc) -> {ok, [{Key, Red} | Acc]} end,
+    ViewArgs = #view_query_args{
+        run_reduce = true,
+        view_name = ViewName,
+        start_key = StartKey,
+        end_key = EndKey
+    },
+    {ok, Rows} = couch_set_view:fold_reduce(Group, View, FoldFun, [], ViewArgs),
+    couch_set_view:release_group(Group),
+    case Rows of
+    [{_Key, {json, RedValue}}] ->
+        ejson:decode(RedValue);
+    [] ->
+        empty
+    end.
+
+
+verify_btrees_1(ValueGenFun) ->
+    GroupPid = couch_set_view:get_group_pid(test_set_name(), ddoc_id()),
+    {ok, Group} = gen_server:call(GroupPid, request_group, infinity),
+    #set_view_group{
+        id_btree = IdBtree,
+        views = Views,
+        index_header = #set_view_index_header{
+            seqs = HeaderUpdateSeqs,
+            abitmask = Abitmask,
+            pbitmask = Pbitmask,
+            cbitmask = Cbitmask
+        }
+    } = Group,
+    etap:is(length(Views), 2, "2 view record in view group"),
+    [View1, View2] = Views,
+    ?etap_match(View1#set_view.reduce_funs,
+                [{<<"test5">>, _}, {<<"test4">>, _}, {<<"test3">>, _}],
+                "First view record has views 5 downto 3"),
+    ?etap_match(View2#set_view.reduce_funs,
+                [{<<"test2">>, _}, {<<"test">>, _}],
+                "Second view record has views 2 downto 1"),
+    #set_view{
+        id_num = 0,
+        btree = View1Btree
+    } = View1,
+    #set_view{
+        id_num = 1,
+        btree = View2Btree
+    } = View2,
+
+    etap:diag("Verifying view group btrees"),
+    ExpectedBitmask = couch_set_view_util:build_bitmask(lists:seq(0, 63)),
+    DbSeqs = couch_set_view_test_util:get_db_seqs(test_set_name(), lists:seq(0, 63)),
+    ExpectedReductionViewBtree1 = [
+        initial_num_docs(),
+        -initial_num_docs(),
+        lists:sum([ValueGenFun(I) || I <- lists:seq(0, initial_num_docs() - 1)])
+    ],
+    ExpectedReductionViewBtree2 = [
+        list_to_binary(integer_to_list(initial_num_docs())),
+        initial_num_docs()
+    ],
+
+    etap:is(
+        couch_set_view_test_util:full_reduce_id_btree(Group, IdBtree),
+        {ok, {initial_num_docs(), ExpectedBitmask}},
+        "Id Btree has the right reduce value"),
+    etap:is(
+        couch_set_view_test_util:full_reduce_view_btree(Group, View1Btree),
+        {ok, {initial_num_docs(), ExpectedReductionViewBtree1, ExpectedBitmask}},
+        "View1 Btree has the right reduce value"),
+    etap:is(
+        couch_set_view_test_util:full_reduce_view_btree(Group, View2Btree),
+        {ok, {initial_num_docs(), ExpectedReductionViewBtree2, ExpectedBitmask}},
+        "View2 Btree has the right reduce value"),
+
+    etap:is(HeaderUpdateSeqs, DbSeqs, "Header has right update seqs list"),
+    etap:is(Abitmask, ExpectedBitmask, "Header has right active bitmask"),
+    etap:is(Pbitmask, 0, "Header has right passive bitmask"),
+    etap:is(Cbitmask, 0, "Header has right cleanup bitmask"),
+
+    etap:diag("Verifying the Id Btree"),
+    MaxPerPart = initial_num_docs() div num_set_partitions(),
+    {ok, _, {_, _, _, IdBtreeFoldResult}} = couch_set_view_test_util:fold_id_btree(
+        Group,
+        IdBtree,
+        fun(Kv, _, {P0, I0, C0, It}) ->
+            case C0 >= MaxPerPart of
+            true ->
+                P = P0 + 1,
+                I = P,
+                C = 1;
+            false ->
+                P = P0,
+                I = I0,
+                C = C0 + 1
+            end,
+            true = (P < num_set_partitions()),
+            ExpectedKv = {
+                <<P:16, (doc_id(I))/binary>>,
+                {P, [{0, [doc_id(I), 1]}, {1, doc_id(I)}]}
+            },
+            case ExpectedKv =:= Kv of
+            true ->
+                ok;
+            false ->
+                etap:bail("Id Btree has an unexpected KV at iteration " ++ integer_to_list(It))
+            end,
+            {ok, {P, I + num_set_partitions(), C, It + 1}}
+        end,
+        {0, 0, 0, 0}, []),
+    etap:is(IdBtreeFoldResult, initial_num_docs(),
+        "Id Btree has " ++ integer_to_list(initial_num_docs()) ++ " entries"),
+    etap:diag("Verifying the View1 Btree"),
+    {ok, _, View1BtreeFoldResult} = couch_set_view_test_util:fold_view_btree(
+        Group,
+        View1Btree,
+        fun(Kv, _, Acc) ->
+            PartId = Acc rem 64,
+            ExpectedKv = {{[doc_id(Acc), 1], doc_id(Acc)}, {PartId, ValueGenFun(Acc)}},
+            case ExpectedKv =:= Kv of
+            true ->
+                ok;
+            false ->
+                etap:bail("View1 Btree has an unexpected KV at iteration " ++ integer_to_list(Acc))
+            end,
+            {ok, Acc + 1}
+        end,
+        0, []),
+    etap:is(View1BtreeFoldResult, initial_num_docs(),
+        "View1 Btree has " ++ integer_to_list(initial_num_docs()) ++ " entries"),
+    etap:diag("Verifying the View2 Btree"),
+    {ok, _, View2BtreeFoldResult} = couch_set_view_test_util:fold_view_btree(
+        Group,
+        View2Btree,
+        fun(Kv, _, Acc) ->
+            PartId = Acc rem 64,
+            ExpectedKv = {{doc_id(Acc), doc_id(Acc)}, {PartId, ValueGenFun(Acc)}},
+            case ExpectedKv =:= Kv of
+            true ->
+                ok;
+            false ->
+                etap:bail("View2 Btree has an unexpected KV at iteration " ++ integer_to_list(Acc))
+            end,
+            {ok, Acc + 1}
+        end,
+        0, []),
+    etap:is(View2BtreeFoldResult, initial_num_docs(),
+        "View2 Btree has " ++ integer_to_list(initial_num_docs()) ++ " entries").

commit ec6ac751563ac77b252c623f3ad98bcdc52ef32a

MB-8877 Fix builtin reduces for subset development views
The regression was introduced in the commit
912b8667560a275a652a2ff0d9d9264ed38e9328:

"MB-7978 Fixes to view tree with multiple reducers"

It only affects 2.1.0, 2.1.1 and 2.2.0 releases. The 3.0 (master branch)
is unaffected, because the same view engine is used for both development
and production views, unlike previous releases.

Change-Id: Id2161d98b015a4fb8a841ded38affad31e4a8c56
Reviewed-on: http://review.couchbase.org/28429
Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>
Reviewed-by: Volker Mische <volker.mische@gmail.com>

src/couchdb/couch_view_mapreduce.erl
@@ -212,7 +212,13 @@ group_reductions_results(List) ->
 
 builtin_reduce(_Re, [], _KVs, Acc) ->
     {ok, lists:reverse(Acc)};
-builtin_reduce(Re, [<<"_sum", _/binary>> | BuiltinReds], KVs, Acc) ->
+builtin_reduce(Re, [<<"_sum", _/binary>> | BuiltinReds], KVs0, Acc) ->
+    case Re of
+    reduce ->
+        KVs = [{K, ?JSON_DECODE(V)} || {K, V} <- KVs0];
+    rereduce ->
+        KVs = KVs0
+    end,
     Sum = builtin_sum_rows(KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Sum | Acc]);
 builtin_reduce(reduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
@@ -221,7 +227,13 @@ builtin_reduce(reduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
 builtin_reduce(rereduce, [<<"_count", _/binary>> | BuiltinReds], KVs, Acc) ->
     Count = builtin_sum_rows(KVs),
     builtin_reduce(rereduce, BuiltinReds, KVs, [Count | Acc]);
-builtin_reduce(Re, [<<"_stats", _/binary>> | BuiltinReds], KVs, Acc) ->
+builtin_reduce(Re, [<<"_stats", _/binary>> | BuiltinReds], KVs0, Acc) ->
+    case Re of
+    reduce ->
+        KVs = [{K, ?JSON_DECODE(V)} || {K, V} <- KVs0];
+    rereduce ->
+        KVs = KVs0
+    end,
     Stats = builtin_stats(Re, KVs),
     builtin_reduce(Re, BuiltinReds, KVs, [Stats | Acc]);
 builtin_reduce(_Re, [InvalidBuiltin | _BuiltinReds], _KVs, _Acc) ->