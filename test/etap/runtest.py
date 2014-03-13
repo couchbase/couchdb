77be43094bf664effb81af7abc7e73af716dd866
Sarath Lakshman <sarathlakshman@slynux.com>
2014-03-13 15:41:33 +0530

    MB-10447 Add a wrapper script for running unit tests
    The current CouchDB unit tests uses perl utility program prove
    for executing and parsing etap test output. A shell script is
    used to set the erlang environment variables required by the
    tests. On windows platform, build system cannot run shell script
    as well as perl scripts.
    
    Added a python script that can perform parsing of etap test output
    and also set necessary environment variables. CMake test framework
    can make use of this script to implement cmake tests for CouchDB on
    all platforms.
    
    The script can be executed as follows:
    ./runtest.py -p $COUCHDB_PATH -m $COUCHDB_PATH/src/couch_set_view,$COUCHDB_PATH/src/couch_set_view/test -t test/02-old-index-cleanup.t
    
    Change-Id: Ic8d90815901466322c32e6578fb79081b6c69cad
    Reviewed-on: http://review.couchbase.org/34466
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ test/etap/runtest.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# Description: Wrapper script for running CouchDB unit tests
+
+import sys
+import getopt
+import os
+import subprocess
+import re
+
+TEST_COUNT_RE = r"^1\.\.(\d+)"
+TEST_OK_RE = r"(^ok)\b"
+TEST_NOT_OK_RE = r"(^not ok)\b"
+MODULES = ["test/etap", "src/mapreduce", "src/couch_index_merger", "src/couch_view_parser"]
+
+def usage():
+    print "Usage: %s -p builddir -m module_dir_paths -t testfile" %sys.argv[0]
+    print
+
+def run_test(testfile):
+    test_total = -1
+    test_passed = 0
+    exit_status = 0
+    count_re = re.compile(TEST_COUNT_RE)
+    ok_re = re.compile(TEST_OK_RE)
+    not_ok_re = re.compile(TEST_NOT_OK_RE)
+    s = subprocess.Popen(['escript', testfile], stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    while True:
+        line = s.stdout.readline()
+        if line:
+            if ok_re.match(line):
+                test_passed += 1
+                print line,
+            elif not_ok_re.match(line):
+                print line,
+            else:
+                count = count_re.match(line)
+                if count:
+                    test_total = int(count.group(1))
+        else:
+            break
+
+    if test_total == test_passed:
+        exit_status = 0
+    else:
+        exit_status = 1
+        err = s.stderr.read()
+        if err:
+            print err
+
+    if test_total > 0:
+        print "%d/%d tests passed" %(test_passed, test_total)
+
+    return exit_status
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:m:t:h", \
+                    ["paths=", "modules=", "test=", "help"])
+    except getopt.GetoptError, err:
+        print err
+        usage()
+        sys.exit(2)
+
+    if not opts:
+        usage()
+        sys.exit(2)
+
+    path = None
+    test = None
+    modules = None
+
+    for opt, arg in opts:
+        if opt in ("-p", "--path"):
+            path = arg
+        if opt in ("-m", "--modules"):
+            modules = arg
+        elif opt in ("-t", "--test"):
+            test = arg
+        elif opt in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+
+    if modules:
+        MODULES += modules.split(",")
+
+    if path:
+        erl_libs = os.path.join(path, "src")
+        env = os.getenv("ERL_LIBS")
+        if env:
+            erl_libs = env + ":" + erl_libs
+
+        os.putenv("ERL_LIBS", erl_libs)
+        flags = map(lambda x: "-pa %s" %os.path.join(path, x), MODULES)
+        erl_flags = " ".join(flags)
+        env = os.getenv("ERL_FLAGS")
+        if env:
+            erl_flags = env + " " + erl_flags
+
+        os.putenv("ERL_FLAGS", erl_flags)
+
+    if test:
+        sys.exit(run_test(test))
+    else:
+        sys.exit("ERROR: No test specified")
