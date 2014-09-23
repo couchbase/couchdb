efc2e928e451ad0b606b1a6e1fbfd72d395eea61
Volker Mische <vmx@couchbase.com>
2014-09-23 18:00:47 +0200

    MB-12112 Simplify unit test runner
    The unit test runner `runtest.py` needed the source as well as the
    build directory of couchdb. This didn't work well as ERL_LIBS picks
    the first application it encounters without checking if there might
    be one which contains missing files later in the path.
    
    The build system was changes so that the build directory also includes
    the header files, hence only the information about the build path is
    needed.
    
    The `source` parameter got renamed to `libsdirs` and now takes a
    directory as input that will be added to the ERL_LIBS environment
    variable. This enables geocouch to use the same test runner.
    
    All the subdirectories from the supplied couchdb build directory are
    now added to erl_flags. This way you don't need to supply as many
    modules as previously.
    
    Change-Id: I07d5c40071ea85342ef72d2e64acf9feda0fb25c
    Reviewed-on: http://review.couchbase.org/41581
    Reviewed-by: Sarath Lakshman <sarath@couchbase.com>
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- test/etap/runtest.py
+++ test/etap/runtest.py
@@ -10,10 +10,11 @@ import re
 TEST_COUNT_RE = r"^1\.\.(\d+)"
 TEST_OK_RE = r"(^ok)\b"
 TEST_NOT_OK_RE = r"(^not ok)\b"
-MODULES = ["test/etap", "src/mapreduce", "src/couch_index_merger", "src/couch_view_parser"]
+
+MODULES = ["../test/etap"]
 
 def usage():
-    print "Usage: %s -p builddir -s sourcedir -m module_dir_paths" \
+    print "Usage: %s -p builddir -l erl_libs_dir -m module_dir_paths" \
     " -t testfile [ -v ] [ -c couchstore_install_path ]" %sys.argv[0]
     print
 
@@ -59,8 +60,8 @@ def run_test(testfile, verbose = False):
 
 if __name__ == '__main__':
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "p:s:m:t:hvc:", \
-                    ["paths=", "source=", "modules=", "test=", "help",
+        opts, args = getopt.getopt(sys.argv[1:], "p:l:m:t:hvc:", \
+                    ["path=", "libsdir=", "modules=", "test=", "help",
                      "verbose", "couchstore-installdir="])
     except getopt.GetoptError, err:
         print err
@@ -72,7 +73,7 @@ if __name__ == '__main__':
         sys.exit(2)
 
     path = None
-    source = None
+    libsdir = None
     test = None
     modules = None
     verbose = False
@@ -81,8 +82,8 @@ if __name__ == '__main__':
     for opt, arg in opts:
         if opt in ("-p", "--path"):
             path = arg
-        if opt in ("-s", "--source"):
-            source = arg
+        if opt in ("-l", "--libsdir"):
+            libsdir = arg
         if opt in ("-m", "--modules"):
             modules = arg
         elif opt in ("-t", "--test"):
@@ -98,32 +99,48 @@ if __name__ == '__main__':
     if modules:
         MODULES += modules.split(",")
 
+    erl_libs = []
+    env = os.getenv("ERL_LIBS")
+    if env:
+        erl_libs.append(env)
+
+    if libsdir:
+        erl_libs.append(libsdir)
+
     # Erlang modules expect posix path format
     # which is respected by all platforms
     # including Windows. So replace '\' by '/'
     # Ensure spaces escaped by '\' are intact,
     # since directory names might contain space
-    if path:
-        erl_libs = os.path.join(path, "src")
-        env = os.getenv("ERL_LIBS")
-        if env:
-            erl_libs = env + ":" + erl_libs
-        erl_libs = re.sub(r'\\(\S)', r'/\1', erl_libs)
-        os.putenv("ERL_LIBS", erl_libs)
+    pattern = re.compile(r'\\(\S)')
 
-        source = os.path.join(source, "src")
-        flags = map(lambda x: "-pa %s" %os.path.join(path, x), MODULES)
-        flags += map(lambda x: "-pa %s" %os.path.join(source, x), MODULES)
-        erl_flags = " ".join(flags)
+    erl_flags = ""
+    env = os.getenv("ERL_FLAGS")
+    if env:
+        erl_flags = env
+
+    if path:
+        erl_libs.append(path)
+
+        # As some modules are not proper OTP apps, just add all dirs as
+        # ERL_FLAGS paths as well
+        MODULES += [d for d in os.listdir(path)
+                   if os.path.isdir(os.path.join(path, d))]
+
+        flags = map(lambda x: " %s" %os.path.join(path, x), MODULES)
+        erl_flags = "-pa" + " ".join(flags)
         env = os.getenv("ERL_FLAGS")
-        if env:
-            erl_flags = env + " " + erl_flags
-        erl_flags = re.sub(r'\\(\S)', r'/\1', erl_flags)
+        erl_flags += " -pa" + " ".join(flags)
+        erl_flags = pattern.sub(r'/\1', erl_flags)
         os.putenv("ERL_FLAGS", erl_flags)
 
-        if verbose:
-            print('ERL_LIBS="{}"'.format(erl_libs))
-            print('ERL_FLAGS="{}"'.format(erl_flags))
+    erl_libs = os.pathsep.join(erl_libs)
+    erl_libs = pattern.sub(r'/\1', erl_libs)
+    os.putenv("ERL_LIBS", erl_libs)
+
+    if verbose:
+        print('ERL_LIBS="{}"'.format(erl_libs))
+        print('ERL_FLAGS="{}"'.format(erl_flags))
 
     if couchstore_path:
         env = os.getenv("PATH")
