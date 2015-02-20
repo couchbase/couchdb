ff9ef9689f26f7131bcb515059e4056baa54e88f
Volker Mische <vmx@couchbase.com>
2015-02-20 18:15:46 +0100

    MB-13556 Remove ICU port driver
    The ICU port driver is used by the old couchdb views for collation
    and is no longer used by the set views. The collation happens within
    a NIF now.
    
    Change-Id: Ia13709835ea05fdfabd69a615619c5fb1556c3ed
    Reviewed-on: http://review.couchbase.org/47125
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Nimish Gupta <nimish@couchbase.com>

--- src/couchdb/couch_ejson_compare.erl
+++ src/couchdb/couch_ejson_compare.erl
@@ -36,16 +36,11 @@ init() ->
 
 -spec less(EJsonKey1::term(), EJsonKey2::term()) -> -1 .. 1.
 less(A, B) ->
-    try
-        case less_ejson_nif(A, B) of
-        {error, _Reason} = Error ->
-            throw(Error);
-        Else ->
-            Else
-        end
-    catch
-    error:badarg ->
-        less_erl(A, B)
+    case less_ejson_nif(A, B) of
+    {error, _Reason} = Error ->
+        throw(Error);
+    Else ->
+        Else
     end.
 
 
@@ -65,66 +60,6 @@ less_ejson_nif(_, _) ->
 
 less_json_nif(_, _) ->
     erlang:nif_error(ejson_compare_nif_not_loaded).
-
-
-less_erl(A,A)                                 -> 0;
-
-less_erl(A,B) when is_atom(A), is_atom(B)     -> convert(atom_sort(A) - atom_sort(B));
-less_erl(A,_) when is_atom(A)                 -> -1;
-less_erl(_,B) when is_atom(B)                 -> 1;
-
-less_erl(A,B) when is_number(A), is_number(B) -> convert(A - B);
-less_erl(A,_) when is_number(A)               -> -1;
-less_erl(_,B) when is_number(B)               -> 1;
-
-less_erl(A,B) when is_binary(A), is_binary(B) -> couch_util:collate(A,B);
-less_erl(A,_) when is_binary(A)               -> -1;
-less_erl(_,B) when is_binary(B)               -> 1;
-
-less_erl(A,B) when is_list(A), is_list(B)     -> less_list(A,B);
-less_erl(A,_) when is_list(A)                 -> -1;
-less_erl(_,B) when is_list(B)                 -> 1;
-
-less_erl({A},{B}) when is_list(A), is_list(B) -> less_props(A,B);
-less_erl({A},_) when is_list(A)               -> -1;
-less_erl(_,{B}) when is_list(B)               -> 1.
-
-atom_sort(null) -> 1;
-atom_sort(false) -> 2;
-atom_sort(true) -> 3.
-
-less_props([], [_|_]) ->
-    -1;
-less_props(_, []) ->
-    1;
-less_props([{AKey, AValue}|RestA], [{BKey, BValue}|RestB]) ->
-    case couch_util:collate(AKey, BKey) of
-    0 ->
-        case less_erl(AValue, BValue) of
-        0 ->
-            less_props(RestA, RestB);
-        Result ->
-            Result
-        end;
-    Result ->
-        Result
-    end.
-
-less_list([], [_|_]) ->
-    -1;
-less_list(_, []) ->
-    1;
-less_list([A|RestA], [B|RestB]) ->
-    case less_erl(A,B) of
-    0 ->
-        less_list(RestA, RestB);
-    Result ->
-        Result
-    end.
-
-convert(N) when N < 0 -> -1;
-convert(N) when N > 0 ->  1;
-convert(_)            ->  0.
 
 
 get_raw_json({json, RawJson}) ->
