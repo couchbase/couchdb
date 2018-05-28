0d306ad1fa5a6becea2712bdaf3cf6c027c82a17
Aman Achpal <aman.achpal@couchbase.com>
2018-05-28 21:42:04 +0530

    MB-27549 Fix flush
    Design documents with the same View functions and Options are known
    as aliases. If there are multiple aliases, they point to the same
    couch_set_view_group gen_server, and share the same signature.
    While flushing, we get the group_server for each {SetName, Sig} pair,
    and shutdown the group_server. If there are aliases of a DDoc, then
    we attempt to shutdown the same group_server multiple times, which fails
    with noproc. This change catches the noproc exception.
    
    Change-Id: I5240d03486fae1b41e2e86aad94064cffb1951b9
    Reviewed-on: http://review.couchbase.org/94828
    Reviewed-by: Harsha H S
    Tested-by: Harsha H S

--- src/couch_set_view/src/couch_set_view.erl
+++ src/couch_set_view/src/couch_set_view.erl
@@ -977,7 +977,11 @@ handle_call({before_database_delete, SetName}, _From, Server) ->
         fun({_SetName, {_DDocId, Sig}}) ->
             case ets:lookup(Server#server.sig_to_pid_ets, {SetName, Sig}) of
             [{_, Pid}] when is_pid(Pid) ->
-                gen_server:call(Pid, before_master_delete);
+                try
+                    gen_server:call(Pid, before_master_delete)
+                catch
+                    exit:{noproc, _Reason} -> ok;
+                end;
             _ ->
                 ok
             end
