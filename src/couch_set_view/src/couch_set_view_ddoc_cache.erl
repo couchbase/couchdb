c1c1978e3c714b9bc4d88e56ad5e8dd8ed7d71c9
AnkitPrabhu <ankit.prabhu@couchbase.com>
2019-09-26 12:46:07 +0530

    MB-35855: Audit view definition for view query request
    Change-Id: I707ecedcd2c5b0be0ff224804f046ed6f67ef304
    Reviewed-on: http://review.couchbase.org/115386
    Reviewed-by: Suraj Naik <suraj.naik@couchbase.com>
    Tested-by: <ankit.prabhu@couchbase.com>

--- src/couch_set_view/src/couch_set_view_ddoc_cache.erl
+++ src/couch_set_view/src/couch_set_view_ddoc_cache.erl
@@ -17,7 +17,7 @@
 
 % public API
 -export([start_link/0]).
--export([get_ddoc/2]).
+-export([get_ddoc/2, get_view/3]).
 
 % gen_server API
 -export([init/1, handle_call/3, handle_info/2, handle_cast/2]).
@@ -28,8 +28,12 @@
 
 -define(BY_DDOC_ID, set_view_by_ddoc_id_ets).
 -define(BY_ATIME, set_view_by_atime_ets).
+-define(BY_VIEW_ID, set_view_by_view_id_ets).
+-define(BY_ATIME_VIEW, set_view_by_atime_view_ets).
 
 -record(state, {
+    view_caching_enabled = false,
+    cached_view_size = 0,
     max_cache_size = 0,
     byte_size = 0,
     db_notifier = nil
@@ -39,6 +43,8 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+view_caching_enabled() ->
+    gen_server:call(?MODULE, view_caching_enabled).
 
 get_ddoc(SetName, DDocId) ->
     case ets:lookup(?BY_DDOC_ID, {SetName, DDocId}) of
@@ -65,22 +71,61 @@ get_ddoc(SetName, DDocId) ->
         end
     end.
 
+get_view(undefined, _, _) ->
+    undefined;
+get_view(_, undefined, _) ->
+    undefined;
+get_view(_, _, undefined) ->
+    undefined;
+get_view(SetName, DDocId, ViewId) ->
+    case view_caching_enabled() of
+    true ->
+        case ets:lookup(?BY_VIEW_ID, {SetName, DDocId, ViewId}) of
+        [{_, _ATime, View, _DDocSize}] ->
+            ok = gen_server:cast(?MODULE, {cache_hit_view, SetName, DDocId, ViewId}),
+            View;
+        [] ->
+            case get_ddoc(SetName, DDocId) of
+            {ok, DDoc} ->
+                try
+                    ViewList = couch_util:get_view_list(DDoc#doc.body),
+                    View = couch_util:get_value(ViewId, ViewList),
+                    ok = gen_server:call(?MODULE, {add_view, SetName, DDocId, ViewId, View}, infinity),
+                    View
+                catch
+                    _:_ ->
+                        undefined
+                end;
+            _ ->
+                undefined
+            end;
+        _ ->
+            undefined
+        end;
+    _ ->
+        undefined
+    end.
 
 init(_) ->
     ?BY_DDOC_ID = ets:new(?BY_DDOC_ID,
                           [set, protected, named_table, {read_concurrency, true}]),
     ?BY_ATIME = ets:new(?BY_ATIME,
                         [ordered_set, private, named_table]),
+    ?BY_VIEW_ID = ets:new(?BY_VIEW_ID,
+                          [set, protected, named_table, {read_concurrency, true}]),
+    ?BY_ATIME_VIEW = ets:new(?BY_ATIME_VIEW,
+                        [ordered_set, private, named_table]),
     process_flag(trap_exit, true),
     ok = couch_config:register(fun handle_config_change/3),
     {ok, Notifier} = couch_db_update_notifier:start_link(fun handle_db_event/1),
     MaxSize = couch_config:get("set_views", "ddoc_cache_size", "1048576"),
+    CacheViews = couch_config:get("set_views", "cache_views_def", "false"),
     State = #state{
+        view_caching_enabled = list_to_atom(CacheViews),
         db_notifier = Notifier,
         max_cache_size = list_to_integer(MaxSize)
     },
     {ok, State}.
-
 
 handle_call({add_ddoc, SetName, DDoc, DDocSize}, From, State) ->
     gen_server:reply(From, ok),
@@ -101,8 +146,31 @@ handle_call({add_ddoc, SetName, DDoc, DDocSize}, From, State) ->
         true = ets:insert(?BY_DDOC_ID, {Key, Now, DDoc, DDocSize}),
         NewSize = State#state.byte_size + DDocSize - OldDDocSize
     end,
-    CacheSize = free_old_entries(State#state.max_cache_size, NewSize),
+    CacheSize = free_old_entries(State#state.max_cache_size, NewSize, ?BY_ATIME, ?BY_DDOC_ID),
     {noreply, State#state{byte_size = CacheSize}};
+
+handle_call({add_view, SetName, DDocId, ViewId, View}, From, State) ->
+    gen_server:reply(From, ok),
+    ViewSize = erlang:external_size(View),
+    Key = {SetName, DDocId, ViewId},
+    case ets:lookup(?BY_VIEW_ID, Key) of
+    [] ->
+        Now = os:timestamp(),
+        true = ets:insert(?BY_ATIME_VIEW, {Now, Key, ViewSize}),
+        true = ets:insert(?BY_VIEW_ID, {Key, Now, View, ViewSize}),
+        NewSize = State#state.cached_view_size + ViewSize;
+    [{_, ATime, _View, OldViewSize}] ->
+        Now = os:timestamp(),
+        true = ets:delete(?BY_ATIME_VIEW, ATime),
+        true = ets:insert(?BY_ATIME_VIEW, {Now, Key, OldViewSize}),
+        true = ets:insert(?BY_VIEW_ID, {Key, Now, View, ViewSize}),
+        NewSize = State#state.cached_view_size + ViewSize - OldViewSize
+    end,
+    CacheSize = free_old_entries(State#state.max_cache_size, NewSize, ?BY_ATIME_VIEW, ?BY_VIEW_ID),
+    {noreply, State#state{cached_view_size = CacheSize}};
+
+handle_call(view_caching_enabled, _From, State) ->
+    {reply, State#state.view_caching_enabled, State};
 
 handle_call({update_ddoc, SetName, DDoc, DDocSize}, From, State) ->
     gen_server:reply(From, ok),
@@ -115,21 +183,34 @@ handle_call({update_ddoc, SetName, DDoc, DDocSize}, From, State) ->
         true = ets:insert(?BY_ATIME, {ATime, Key, DDocSize}),
         true = ets:insert(?BY_DDOC_ID, {Key, ATime, DDoc, DDocSize}),
         NewSize = State#state.byte_size + DDocSize - OldDDocSize,
-        CacheSize = free_old_entries(State#state.max_cache_size, NewSize),
+        CacheSize = free_old_entries(State#state.max_cache_size, NewSize, ?BY_ATIME, ?BY_DDOC_ID),
         {noreply, State#state{byte_size = CacheSize}}
     end;
 
+handle_call({update_view, SetName, DDoc}, From, State) ->
+    gen_server:reply(From, ok),
+    Entries = ets:match_object(?BY_VIEW_ID, {{SetName, DDoc#doc.id, '_'}, '_', '_', '_'}),
+    NewViewSize = lists:foldl(fun({Key, ATime, _, OldViewSize}, NewSize) ->
+        true = ets:delete(?BY_VIEW_ID, Key),
+        true = ets:delete(?BY_ATIME_VIEW, ATime),
+        NewSize - OldViewSize
+    end, State#state.cached_view_size, Entries),
+    {noreply, State#state{cached_view_size = NewViewSize}};
+
 handle_call({delete_ddoc, SetName, Id}, From, State) ->
     gen_server:reply(From, ok),
+    NewViewSize = delete_matched_object_view(SetName, Id, '_', State#state.cached_view_size),
+
     Key = {SetName, Id},
     case ets:lookup(?BY_DDOC_ID, Key) of
     [] ->
-        {noreply, State};
+        {noreply, State#state{cached_view_size = NewViewSize}};
     [{_, ATime, _DDoc, DDocSize}] ->
         true = ets:delete(?BY_DDOC_ID, Key),
         true = ets:delete(?BY_ATIME, ATime),
         NewSize = State#state.byte_size - DDocSize,
-        {noreply, State#state{byte_size = NewSize}}
+        {noreply, State#state{byte_size = NewSize,
+                              cached_view_size = NewViewSize}}
     end;
 
 handle_call({set_deleted, SetName}, From, State) ->
@@ -139,13 +220,27 @@ handle_call({set_deleted, SetName}, From, State) ->
         true = ets:delete(?BY_DDOC_ID, Key),
         true = ets:delete(?BY_ATIME, ATime)
     end, Entries),
-    {noreply, State};
+
+    NewSize = delete_matched_object_view(SetName, '_', '_', State#state.cached_view_size),
+    {noreply, State#state{cached_view_size = NewSize}};
 
 handle_call({new_max_cache_size, NewMaxSize}, _From, State) ->
-    Size = free_old_entries(NewMaxSize, State#state.byte_size),
-    {reply, ok, State#state{byte_size = Size}}.
+    DDocSize = free_old_entries(NewMaxSize, State#state.byte_size, ?BY_ATIME, ?BY_DDOC_ID),
+    ViewSize = free_old_entries(NewMaxSize, State#state.cached_view_size, ?BY_ATIME_VIEW, ?BY_VIEW_ID),
+    {reply, ok, State#state{byte_size = DDocSize, cached_view_size = ViewSize}};
 
-
+handle_call({new_view_cache_setting, Value}, _From, State) ->
+    State2 = case Value of
+    false ->
+        ets:delete_all_objects(?BY_ATIME_VIEW),
+        ets:delete_all_objects(?BY_VIEW_ID),
+        State#state{view_caching_enabled = false, cached_view_size = 0};
+    true ->
+        State#state{view_caching_enabled = true};
+    _ ->
+        State
+    end,
+    {reply, ok, State2}.
 
 handle_cast({cache_hit, SetName, DDocId}, State) ->
     Key = {SetName, DDocId},
@@ -158,8 +253,19 @@ handle_cast({cache_hit, SetName, DDocId}, State) ->
         true = ets:insert(?BY_ATIME, {NewATime, Key, DDocSize}),
         true = ets:insert(?BY_DDOC_ID, {Key, NewATime, DDoc, DDocSize})
     end,
+    {noreply, State};
+handle_cast({cache_hit_view, SetName, DDocId, ViewId}, State) ->
+    Key = {SetName, DDocId, ViewId},
+    case ets:lookup(?BY_VIEW_ID, Key) of
+    [] ->
+        ok;
+    [{_, OldATime, View, ViewSize}] ->
+        NewATime = os:timestamp(),
+        true = ets:delete(?BY_ATIME_VIEW, OldATime),
+        true = ets:insert(?BY_ATIME_VIEW, {NewATime, Key, ViewSize}),
+        true = ets:insert(?BY_VIEW_ID, {Key, NewATime, View, ViewSize})
+    end,
     {noreply, State}.
-
 
 handle_info(shutdown, State) ->
     {stop, shutdown, State}.
@@ -176,20 +282,22 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 
-free_old_entries(MaxSize, CurSize) when CurSize =< MaxSize ->
+free_old_entries(MaxSize, CurSize, _, _) when CurSize =< MaxSize ->
     CurSize;
-free_old_entries(MaxSize, CurSize) ->
-    ATime = ets:first(?BY_ATIME),
-    [{_, {_SetName, _DDocId} = Key, DDocSize}] = ets:lookup(?BY_ATIME, ATime),
-    true = ets:delete(?BY_ATIME, ATime),
-    true = ets:delete(?BY_DDOC_ID, Key),
-    free_old_entries(MaxSize, CurSize - DDocSize).
+free_old_entries(MaxSize, CurSize, AtimeTable, IdTable) ->
+    ATime = ets:first(AtimeTable),
+    [{_, Key, Size}] = ets:lookup(AtimeTable, ATime),
+    true = ets:delete(AtimeTable, ATime),
+    true = ets:delete(IdTable, Key),
+    free_old_entries(MaxSize, CurSize - Size, AtimeTable, IdTable).
 
 
 handle_config_change("set_views", "ddoc_cache_size", NewSizeList) ->
     NewSize = list_to_integer(NewSizeList),
-    ok = gen_server:call(?MODULE, {new_max_cache_size, NewSize}, infinity).
-
+    ok = gen_server:call(?MODULE, {new_max_cache_size, NewSize}, infinity);
+handle_config_change("set_views", "cache_views_def", Value) ->
+    Value2 = list_to_atom(Value),
+    ok = gen_server:call(?MODULE, {new_view_cache_setting, Value2}, infinity).
 
 handle_db_event({deleted, DbName}) ->
     case couch_set_view_util:split_set_db_name(DbName) of
@@ -205,7 +313,8 @@ handle_db_event({ddoc_updated, {DbName, DDoc}}) ->
         false ->
             DDoc2 = couch_doc:with_ejson_body(DDoc),
             Size = erlang:external_size(DDoc2),
-            ok = gen_server:call(?MODULE, {update_ddoc, SetName, DDoc2, Size}, infinity);
+            ok = gen_server:call(?MODULE, {update_ddoc, SetName, DDoc2, Size}, infinity),
+            ok = gen_server:call(?MODULE, {update_view, SetName, DDoc2}, infinity);
         true ->
             ok = gen_server:call(?MODULE, {delete_ddoc, SetName, DDoc#doc.id}, infinity)
         end;
@@ -214,3 +323,11 @@ handle_db_event({ddoc_updated, {DbName, DDoc}}) ->
     end;
 handle_db_event(_) ->
     ok.
+
+delete_matched_object_view(SetName, DDocId, ViewId, CurrValue) ->
+    Entries = ets:match_object(?BY_VIEW_ID, {{SetName, DDocId, ViewId}, '_', '_', '_'}),
+    lists:foldl(fun({Key, ATime, _, OldViewSize}, NewSize) ->
+        true = ets:delete(?BY_VIEW_ID, Key),
+        true = ets:delete(?BY_ATIME_VIEW, ATime),
+        NewSize - OldViewSize
+    end, CurrValue, Entries).
