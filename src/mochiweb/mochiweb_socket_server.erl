43f347f4ed8efb1567205d57cd25a3c395b34f81
Steve Watanabe <steve.watanabe@couchbase.com>
2019-11-26 15:54:12 -0800

    MB-16826 Get rid of flawed applications starts in mochiweb.
    Reapplying change to updated mochiweb source files.
    
    It is flawed because it might lead to deadlocks during server
    shutdown. When application:stop(App) is called, it blocks entire
    application_controller process. So anything that tries to access it
    from inside application being stopped will be blocked. It leads to
    deadlock if such blocked call also blocks application shutdown. And
    this is exactly the case with mochiweb, because corresponding
    application:start calls are executed in supervisor process context
    whenever mochiweb process is started.
    
    These applications are now explicitly started by ns_server during
    bootstrap.
    
    Change-Id: I282308e17e4e00e1636b8cdb21fe0e41a9b5d1dd
    Reviewed-on: http://review.couchbase.org/118570
    Reviewed-by: <ankit.prabhu@couchbase.com>
    Tested-by: <ankit.prabhu@couchbase.com>

--- src/mochiweb/mochiweb_socket_server.erl
+++ src/mochiweb/mochiweb_socket_server.erl
@@ -158,37 +158,13 @@ parse_options([{profile_fun, ProfileFun} | Rest], State) when is_function(Profil
     parse_options(Rest, State#mochiweb_socket_server{profile_fun=ProfileFun}).
 
 
-start_server(F, State=#mochiweb_socket_server{ssl=Ssl, name=Name}) ->
-    ok = prep_ssl(Ssl),
+start_server(F, State=#mochiweb_socket_server{name=Name}) ->
     case Name of
         undefined ->
             gen_server:F(?MODULE, State, []);
         _ ->
             gen_server:F(Name, ?MODULE, State, [])
     end.
-
--ifdef(otp_21).
-check_ssl_compatibility() ->
-    case lists:keyfind(ssl, 1, application:loaded_applications()) of
-        {_, _, V} when V =:= "9.1" orelse V =:= "9.1.1" ->
-            {error, "ssl-" ++ V ++ " (OTP 21.2 to 21.2.2) has a regression and is not safe to use with mochiweb. See https://bugs.erlang.org/browse/ERL-830"};
-        _ ->
-            ok
-    end.
--else.
-check_ssl_compatibility() ->
-    ok.
--endif.
-
-prep_ssl(true) ->
-    ok = mochiweb:ensure_started(crypto),
-    ok = mochiweb:ensure_started(asn1),
-    ok = mochiweb:ensure_started(public_key),
-    ok = mochiweb:ensure_started(ssl),
-    ok = check_ssl_compatibility(),
-    ok;
-prep_ssl(false) ->
-    ok.
 
 ensure_int(N) when is_integer(N) ->
     N;
